9d1fec58a5a324341de095310324993b7feeabb1
yi-yun-liu <sl589632m@gmail.com>
2025-04-22 19:05:41 +0800

    20250422

---
+++ 20250422/call_fun_1137200511022328.py
@@ -0,0 +1,4 @@
+import bus_info_1137200511022328
+
+if __name__ == "__main__":
+    bus_info_1137200511022328.bus_info()
---
+++ 20250422/exe1.py
@@ -0,0 +1,3 @@
+import bus_info_1137200511022328
+     if __name__=='_main_':
+        bus_info_1137200511022328.bus_info()
